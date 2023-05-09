b9a52276d97ccee5e0e90b378cce7457c9de522e
anddreluis2 <anddreluis98@gmail.com>
2023-05-09 15:52:49 -0300

    added the first components

---
+++ src/components/stepper.tsx
@@ -0,0 +1,44 @@
+export default function Stepper() {
+  return (
+    <div className="px-72 w-full justify-around flex">
+      <div className="flex justify-center items-center flex-col">
+        <div className="h-9 w-9 rounded-full border border-blue">
+          <span className="justify-center text-blue font-bold flex mt-1">
+            1
+          </span>
+        </div>
+        <div className="flex pt-3 justify-around text-sm text-gray">
+          Dados da cobrança
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col">
+        <div className="h-9 w-9 rounded-full border border-gray">
+          <span className="justify-center text-gray font-bold flex mt-1">
+            2
+          </span>
+        </div>
+        <div className="flex pt-3 justify-around text-gray text-sm">
+          Juros e multa
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col">
+        <div className="h-9 w-9 rounded-full border border-gray">
+          <span className="justify-center text-gray font-bold flex mt-1">
+            3
+          </span>
+        </div>
+        <div className="flex pt-3 justify-around text-sm text-gray">
+          Dados do cliente
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col">
+        <div className="h-9 w-9 rounded-full border border-gray">
+          <span className="justify-center text-gray font-bold flex mt-1">
+            4
+          </span>
+        </div>
+        <div className="flex pt-3 justify-around text-sm text-gray">Resumo</div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      blue: "#004BFF",
+      gray: "#696969",
+    },
+    boxShadow: {
+      inactive: "0px 2px 4px 2px rgba(0, 0, 0, 0.15)",
+      interaction: "0px 2px 6px 4px rgba(0, 0, 0, 0.15)",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
