25cc983b75a26f7b435dc13bb7aeffca3fd621c9
anddreluis2 <anddreluis98@gmail.com>
2023-05-10 12:08:41 -0300

    added main div arround

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,55 +3,66 @@ import Card from "@/components/card";
 import GroupedRadio from "@/components/groupedRadio";
 import Stepper from "@/components/stepper";
 import TabSlider from "@/components/tabSlider";
+import { FaArrowLeft, FaBackward } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#FFF] px-64 py-14">
-      <Stepper />
-      <div className="flex pt-7 justify-between">
-        <div className="flex flex-col">
-          <h1 className="font-bold text-gray-800 pb-6">O QUE COBRAR?</h1>
-          <div className="rounded-lg w-48 relative">
-            <div className="bottom-0 left-0 absolute w-9 h-9 bg-blue rounded-lg flex items-center justify-center text-[#FFF] font-bold">
-              R$
+    <main className="min-h-screen bg-[#EEEEEE] py-14">
+      <div className="w-[95%] py-4 mx-auto max-w-[1140px]">
+        <button className="items-center uppercase font-semibold text-blue py-4 gap-2 flex">
+          <FaArrowLeft /> Voltar
+        </button>
+        <h3 className="text-gray uppercase font-semibold text-sm">cobranca</h3>
+      </div>
+      <section className="bg-[#FFF] py-7 rounded-lg w-[95%] mx-auto px-16 max-w-[1140px]">
+        <Stepper />
+        <div className="flex pt-7 justify-between">
+          <div className="flex flex-col">
+            <h1 className="font-bold text-gray-800 pb-6 uppercase">
+              o que cobrar?
+            </h1>
+            <div className="rounded-lg w-48 relative">
+              <div className="bottom-0 left-0 absolute w-9 h-9 bg-blue rounded-lg flex items-center justify-center text-[#FFF] font-bold">
+                R$
+              </div>
+              <input
+                min="0"
+                placeholder="0,00"
+                className="w-48 border placeholder:font-semibold placeholder:text-gray border-blue h-9 px-10 rounded-lg"
+              />
             </div>
-            <input
-              min="0"
-              placeholder="0,00"
-              className="w-48 border placeholder:font-semibold placeholder:text-gray border-blue h-9 px-10 rounded-lg"
-            />
+            <div className="pt-5 pb-2">
+              <h3 className="font-semibold text-md text-gray">
+                Descrição da cobrança (Opcional)
+              </h3>
+              <input
+                placeholder="A descrição informada será impressa na fatura."
+                type="text"
+                className="w-96 border border-gray mt-4 placeholder:text-xs placeholder:text-top placeholder:text-gray p-2 pb-[80px] h-28 rounded-lg"
+              />
+            </div>
           </div>
-          <div className="pt-5 pb-2">
-            <h3 className="font-semibold text-md text-gray">
-              Descrição da cobrança (Opcional)
-            </h3>
-            <input
-              placeholder="A descrição informada será impressa na fatura."
-              type="text"
-              className="w-96 border border-gray mt-4 placeholder:text-xs placeholder:text-top placeholder:text-gray p-2 pb-[80px] h-28 rounded-lg"
-            />
-          </div>
+          <Card />
         </div>
-        <Card />
-      </div>
 
-      <div className="pt-7 flex flex-col">
-        <h1 className="font-bold pb-6">QUAL SERÁ A FORMA DE PAGAMENTO?</h1>
-        <div className="">
-          <TabSlider />
+        <div className="pt-7 flex flex-col">
+          <h1 className="font-bold pb-6">QUAL SERÁ A FORMA DE PAGAMENTO?</h1>
+          <div className="">
+            <TabSlider />
+          </div>
+          <div className="">
+            <h3 className="font-bold pb-5">COMO SEU CLIENTE PODERÁ PAGAR?</h3>
+            <GroupedRadio />
+          </div>
         </div>
-        <div className="">
-          <h3 className="font-bold pb-5">COMO SEU CLIENTE PODERÁ PAGAR?</h3>
-          <GroupedRadio />
-        </div>
-      </div>
-      <footer>
-        <div className="flex justify-end text-[#FFF] font-bold rounded-lg pt-10">
-          <button type="submit" className="bg-blue w-48 h-12 rounded-3xl">
-            AVANÇAR
-          </button>
-        </div>
-      </footer>
+        <footer>
+          <div className="flex justify-end text-[#FFF] font-bold rounded-lg pt-10">
+            <button type="submit" className="bg-blue w-48 h-12 rounded-3xl">
+              AVANÇAR
+            </button>
+          </div>
+        </footer>
+      </section>
     </main>
   );
 }
--- src/components/stepper.tsx
+++ src/components/stepper.tsx
@@ -1,44 +1,59 @@
 export default function Stepper() {
   return (
-    <div className="justify-around flex">
-      <div className="flex justify-center items-center flex-col">
-        <div className="h-9 w-9 rounded-full border border-blue">
-          <span className="justify-center text-blue font-bold flex mt-1">
-            1
-          </span>
+    <div className="">
+      <main className="relative max-w-[100%] w-[840px] mx-auto pb-7">
+        <div className="justify-between flex after:absolute after:bg-gray after:block after:h-[2px] after:w-full  after:top-4">
+          <div className="flex justify-center items-center z-50 flex-col relative ">
+            <div className="h-9 w-9 text-center rounded-full border bg-[#fff] border-blue">
+              <span className="justify-center text-blue font-bold flex mt-1">
+                1
+              </span>
+              <span className="flex pt-4 justify-center whitespace-nowrap py-2 text-sm text-gray">
+                Dados da cobrança
+              </span>
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center z-50 flex-col">
+            <div className="h-9 w-9 rounded-full border bg-[#fff] border-gray">
+              <span className="justify-center text-gray font-bold flex mt-1">
+                2
+              </span>
+              <span className="flex pt-4 justify-center whitespace-nowrap text-sm text-gray">
+                Juros e multa
+              </span>
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center z-50 flex-col">
+            <div className="h-9 w-9 rounded-full border bg-[#fff] border-gray">
+              <span className="justify-center text-gray font-bold flex mt-1">
+                3
+              </span>
+              <span className="flex pt-4 justify-center whitespace-nowrap text-sm text-gray">
+                Dados do cliente
+              </span>
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center z-50 flex-col">
+            <div className="h-9 w-9 rounded-full border bg-[#fff] border-gray">
+              <span className="justify-center text-gray font-bold flex mt-1">
+                4
+              </span>
+              <span className="flex pt-4 justify-center whitespace-nowrap text-sm text-gray">
+                Resumo
+              </span>
+            </div>
+          </div>
         </div>
-        <div className="flex pt-3 justify-around text-sm text-gray">
-          Dados da cobrança
-        </div>
-      </div>
-      <div className="flex justify-center items-center flex-col">
-        <div className="h-9 w-9 rounded-full border border-gray">
-          <span className="justify-center text-gray font-bold flex mt-1">
-            2
-          </span>
-        </div>
-        <div className="flex pt-3 justify-around text-gray text-sm">
-          Juros e multa
-        </div>
-      </div>
-      <div className="flex justify-center items-center flex-col">
-        <div className="h-9 w-9 rounded-full border border-gray">
-          <span className="justify-center text-gray font-bold flex mt-1">
-            3
-          </span>
-        </div>
-        <div className="flex pt-3 justify-around text-sm text-gray">
-          Dados do cliente
-        </div>
-      </div>
-      <div className="flex justify-center items-center flex-col">
-        <div className="h-9 w-9 rounded-full border border-gray">
-          <span className="justify-center text-gray font-bold flex mt-1">
-            4
-          </span>
-        </div>
-        <div className="flex pt-3 justify-around text-sm text-gray">Resumo</div>
-      </div>
+      </main>
+      {/* <div className="w-[880px] flex justify-between mx-auto text-sm text-gray pt-2">
+        <div className="">Dados da cobrança</div>
+        <div className="">Juros e multa</div>
+        <div className="">Dados do cliente</div>
+        <div className="">Resumo</div>
+      </div> */}
     </div>
   );
 }
